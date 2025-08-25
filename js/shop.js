7ba612f4ea515ea5a8a681459bf7d5dc82eeeda6
booldook <booldook@gmail.com>
2025-08-25 15:50:08 +0900

    Component 구조 완료

---
+++ js/shop.js
@@ -0,0 +1,29 @@
+const { useState } = React;
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+
+const PrdWrap = ({ children }) => {
+  return (
+    <div className="prd-wrap">
+      <img src="../images/cherries.jpg" className="img" />
+      <h2>title</h2>
+      <div>description</div>
+    </div>
+  );
+};
+
+const Containers = (props) => {
+  return (
+    <div className="containers">
+      <PrdWrap />
+      <PrdWrap />
+      <PrdWrap />
+      <PrdWrap />
+      <PrdWrap />
+      <PrdWrap />
+      <PrdWrap />
+      <PrdWrap />
+    </div>
+  );
+};
+
+root.render(<Containers />);
