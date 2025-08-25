5a50144b0861490eda518b0a61fdda64a1ca72e6
booldook <booldook@gmail.com>
2025-08-25 12:19:58 +0900

    tailwind 적용

--- js/jsx01.js
+++ js/jsx01.js
@@ -1,7 +1,29 @@
 const app = document.querySelector("#app");
 const root = ReactDOM.createRoot(app);
 
-const el = React.createElement("h1", { className: "title" }, "Hello React"); // jsx
-console.log(el);
+// const el = React.createElement("h1", { className: "title" }, "Hello React");
+// console.log(el);
+
+// const data = axios.get("https://jsonplaceholder.typicode.com/users")
+const greeting = "Hello React";
+const users = [
+  { id: 1, name: "홍길동", age: 23 },
+  { id: 2, name: "홍길순", age: 28 },
+  { id: 3, name: "홍길만", age: 22 },
+];
+
+const el = (
+  <>
+    <h1 className="mx-auto text-center">{greeting}</h1>
+    <ul>
+      {users.map((user) => (
+        <li>
+          <span>{user.id}</span> /<span>{user.name}</span> /
+          <span>{user.age}</span>
+        </li>
+      ))}
+    </ul>
+  </>
+);
 
 root.render(el);
